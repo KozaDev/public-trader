24d6108ffbb3c7da140232bcbe3ce0cf733f3cf7
Kacper <kozadevpytania@gmail.com>
2022-09-19 12:21:03 +0200

    Add color for disabled options in Select component

--- src/components/molecules/Select/StyledSelect.js
+++ src/components/molecules/Select/StyledSelect.js
@@ -1,17 +1,21 @@
 import styled from "styled-components";
-import { rgba } from "polished";
+import { rgba, darken } from "polished";
 
 const StyledSelect = styled.select`
   font-size: inherit;
   outline: none;
   border: none;
-  background-color: ${({ theme }) => rgba(theme.colors.primaryBlue, 1)};
+  background-color: ${({ theme }) => theme.colors.primaryBlue};
   padding: 3px;
   color: ${({ theme }) => theme.colors.white};
   border-radius: 8px;
   box-shadow: 0;
   &:hover {
     cursor: pointer;
+  }
+
+  option:disabled {
+    color: ${({ theme }) => darken(0.15, theme.colors.whiteSmoke)};
   }
 `;
 
